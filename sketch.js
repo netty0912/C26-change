9cc7daa3d6fd065dbac8a8bca974ec2daf84aa15
netty <91903076+netty0912@users.noreply.github.com>
2022-03-22 10:28:49 -0600

    Agregamos platform

--- sketch.js
+++ sketch.js
@@ -19,7 +19,8 @@ function setup(){
 
    
     
-    ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,1200,20);
+    platform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -62,4 +63,5 @@ function draw(){
     log5.display();
 
     bird.display();
+    platform.display();
 }
